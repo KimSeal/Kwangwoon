ba36bb8fe806564664b99253d1262fe2f907624c
KimSeal <56949110+KimSeal@users.noreply.github.com>
2023-08-27 01:50:36 +0900

    Delete 2019_1_digital_logical_circuit_1 directory

--- 2019_1_digital_logical_circuit_1/RippleCarryAdder/_XOR/_XOR.v
+++
@@ -1,5 +0,0 @@
-module _XOR(A,B,Y);//XOR function
-	input A,B;
-	output Y;
-	assign Y = A ^ B;//^ equals (+)
-endmodule
--- 2019_1_digital_logical_circuit_1/RippleCarryAdder/_XOR/simulation/modelsim/rtl_work/tb_@x@o@r/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity tb_XOR is
-end tb_XOR;
